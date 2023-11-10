d651e4e131c5e766d9d9d9303e8e8f3c6f233675
Mage <44714464+madebymage@users.noreply.github.com>
2023-11-09 22:57:27 -0800

    Update MageBotV1.py
    incorporated use of consistent f-strings
    
    created separate functions to handle specific tasks, making the code more modular and readable.
    
    While the provided code does not contain complex error handling, incorporating try...except blocks in functions like update_existing_file would be beneficial to catch potential errors and provide user-friendly error messages.

--- MageBotV1.py
+++ MageBotV1.py
@@ -1,89 +1,102 @@
 import subprocess
 import os
 
-ascii_art = """
-                                                                                 
-          ____                                                                   
-        ,'  , `.                                    ,---,.               ___     
-     ,-+-,.' _ |                                  ,'  .'  \            ,--.'|_   
-  ,-+-. ;   , ||                                ,---.' .' |   ,---.    |  | :,'  
- ,--.'|'   |  ;|             ,----._,.          |   |  |: |  '   ,'\   :  : ' :  
-|   |  ,', |  ':  ,--.--.   /   /  ' /   ,---.  :   :  :  / /   /   |.;__,'  /   
-|   | /  | |  || /       \ |   :     |  /     \ :   |    ; .   ; ,. :|  |   |    
-'   | :  | :  |,.--.  .-. ||   | .\  . /    /  ||   :     \'   | |: ::__,'| :    
-;   . |  ; |--'  \__\/: . ..   ; ';  |.    ' / ||   |   . |'   | .; :  '  : |__  
-|   : |  | ,     ," .--.; |'   .   . |'   ;   /|'   :  '; ||   :    |  |  | '.'| 
-|   : '  |/     /  /  ,.  | `---`-'| |'   |  / ||   |  | ;  \   \  /   ;  :    ; 
-;   | |`-'     ;  :   .'   \.'__/\_: ||   :    ||   :   /    `----'    |  ,   /  
-|   ;/         |  ,     .-./|   :    : \   \  / |   | ,'                ---`-'   
-'---'           `--`---'     \   \  /   `----'  `----'                           
-                              `--`-'                                             
+def display_welcome_message():
+    """Display the welcome message with ASCII art."""
+    ascii_art = """
+          ____
+        ,'  , `.                                    ,---,.               ___
+     ,-+-,.' _ |                                  ,'  .'  \            ,--.'|_
+  ,-+-. ;   , ||                                ,---.' .' |   ,---.    |  | :,'
+ ,--.'|'   |  ;|             ,----._,.          |   |  |: |  '   ,'\   :  : ' :
+|   |  ,', |  ':  ,--.--.   /   /  ' /   ,---.  :   :  :  / /   /   |.;__,'  /
+|   | /  | |  || /       \ |   :     |  /     \ :   |    ; .   ; ,. :|  |   |
+'   | :  | :  |,.--.  .-. ||   | .\  . /    /  ||   :     \'   | |: ::__,'| :
+;   . |  ; |--'  \__\/: . ..   ; ';  |.    ' / ||   |   . |'   | .; :  '  : |__
+|   : |  | ,     ," .--.; |'   .   . |'   ;   /|'   :  '; ||   :    |  |  | '.'|
+|   : '  |/     /  /  ,.  | `---`-'| |'   |  / ||   |  | ;  \   \  /   ;  :    ;
+;   | |`-'     ;  :   .'   \.'__/\_: ||   :    ||   :   /    `----'    |  ,   /
+|   ;/         |  ,     .-./|   :    : \   \  / |   | ,'                ---`-'
+'---'           `--`---'     \   \  /   `----'  `----'
+                              `--`-'
 """
+    print(ascii_art)
 
-print(ascii_art)
+def get_user_name():
+    """Get the user's name."""
+    return input("Enter your name: ")
 
-# Taking user input for their name
-name = input("Enter your name: ")
-
-# Checking if the name is "Mage" or "mage"
-if name.lower() == "mage":
+def handle_master_options(name):
+    """Handle options for the master user."""
     print(f"Greetings Master {name}, how can I assist you?")
     print("Options: ")
     print("1. Create ")
     print("2. Update ")
-    
-    # Taking user input for options
+
     option = input("Enter your choice (1 or 2): ")
-    
+
     if option == "1":
-        # Execute command to create and open a new Python file in nano
-        subprocess.run(["nano", "new.py"])
+        create_new_file()
     elif option == "2":
-        # List files in the current directory
-        files = os.listdir(os.getcwd())
-        print("Existing files in the current directory:")
-        for file in files:
-            print(file)
-        
-        # Taking user input for the file to open
-        file_to_open = input("Enter the name of the file to open in nano: ")
-        
-        # Check if the entered file exists in the directory
-        if file_to_open in files:
-            # Execute command to open the selected file in nano
-            subprocess.run(["nano", file_to_open])
-        else:
-            print("File not found. Please try again.")
+        update_existing_file()
     else:
         print("Invalid option. Please try again.")
-else:
-    print(f"""                                                                                        
-                                                                                        
-                ░░░░                                                                    
-                                                                                        
-                                            ██                                          
-                                          ██░░██                                        
-  ░░          ░░                        ██░░░░░░██                            ░░░░      
-                                      ██░░░░░░░░░░██                                    
-                                      ██░░░░░░░░░░██                                    
-                                    ██░░░░░░░░░░░░░░██                                  
-                                  ██░░░░░░██████░░░░░░██                                
-                                  ██░░░░░░██████░░░░░░██                                
-                                ██░░░░░░░░██████░░░░░░░░██                              
-                                ██░░░░░░░░██████░░░░░░░░██                              
-                              ██░░░░░░░░░░██████░░░░░░░░░░██                            
-                            ██░░░░░░░░░░░░██████░░░░░░░░░░░░██                          
-                            ██░░░░░░░░░░░░██████░░░░░░░░░░░░██                          
-                          ██░░░░░░░░░░░░░░██████░░░░░░░░░░░░░░██                        
-                          ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██                        
-                        ██░░░░░░░░░░░░░░░░██████░░░░░░░░░░░░░░░░██                      
-                        ██░░░░░░░░░░░░░░░░██████░░░░░░░░░░░░░░░░██                      
-                      ██░░░░░░░░░░░░░░░░░░██████░░░░░░░░░░░░░░░░░░██                    
-        ░░            ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██                    
-                        ██████████████████████████████████████████                      
-                                                                                        
-                                                                                        
-                                                                                        
-                                                                                        
-                  ░░                                                                    
-Greetings, {name}! Unfortunately we are currently under construction. Please come back later.""")
+
+def create_new_file():
+    """Create and open a new Python file in nano."""
+    subprocess.run(["nano", "new.py"])
+
+def update_existing_file():
+    """Update an existing file by opening it in nano."""
+    files = os.listdir(os.getcwd())
+    print("Existing files in the current directory:")
+    for file in files:
+        print(file)
+
+    file_to_open = input("Enter the name of the file to open in nano: ")
+
+    if file_to_open in files:
+        subprocess.run(["nano", file_to_open])
+    else:
+        print("File not found. Please try again.")
+
+def main():
+    display_welcome_message()
+    name = get_user_name()
+
+    if name.lower() == "mage":
+        handle_master_options(name)
+    else:
+        print(f"""                                                                                        
+
+                ░░░░
+
+                                            ██
+                                          ██░░██
+  ░░          ░░                        ██░░░░░░██                            ░░░░
+                                      ██░░░░░░░░░░██
+                                      ██░░░░░░░░░░██
+                                    ██░░░░░░░░░░░░░░██
+                                  ██░░░░░░██████░░░░░░██
+                                  ██░░░░░░██████░░░░░░██
+                                ██░░░░░░░░██████░░░░░░░░██
+                                ██░░░░░░░░██████░░░░░░░░██
+                              ██░░░░░░░░░░██████░░░░░░░░░░██
+                            ██░░░░░░░░░░░░██████░░░░░░░░░░░░██
+                            ██░░░░░░░░░░░░██████░░░░░░░░░░░░██
+                          ██░░░░░░░░░░░░░░██████░░░░░░░░░░░░░░██
+                          ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██
+                        ██░░░░░░░░░░░░░░░░██████░░░░░░░░░░░░░░░░██
+                        ██░░░░░░░░░░░░░░░░██████░░░░░░░░░░░░░░░░██
+                      ██░░░░░░░░░░░░░░░░░░██████░░░░░░░░░░░░░░░░░░██
+        ░░            ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██
+                        ██████████████████████████████████████████
+
+
+
+
+                  ░░
+Greetings, {name}! Unfortunately, we are currently under construction. Please come back later.""")
+
+if __name__ == "__main__":
+    main()
